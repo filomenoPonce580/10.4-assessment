24ffb64be96e78e6b693bd0d5b6ea555b117eceb
filomenoponce <filomenoponce580@gmail.com>
2023-03-13 14:21:12 -0700

    fixed test. parenthesis in wrong place

--- test/solution.test.js
+++ test/solution.test.js
@@ -1,30 +1,27 @@
 const findStudentByName = require("../src/solution")
-const expect = require("chai").expect;
+const {expect} = require("chai");
+const input = [
+            { name: "Leo Yeon-Joo", score: 8.9 },
+            { name: "Morgan Sutton", score: 7.4 },
+            { name: "Natalee Vargas", score: 9.2 }
+            ];
 
-describe("findStudentByName", ()=>{
-    it("finds student object based on the input name"), ()=>{
-        const inputArray = [
-            { name: "Leo Yeon-Joo", score: 8.9 },
-            { name: "Morgan Sutton", score: 7.4 },
-            { name: "Natalee Vargas", score: 9.2 }
-        ];
+
+describe("findStudentByName", () => {
+    it("finds student object based on the input name", () =>{
         const expected = { name: "Leo Yeon-Joo", score: 8.9}
-        const actual = findStudentByName(inputArray, "Leo Yeon-Joo")
+        const actual = findStudentByName(input, "Leo Yeon-Joo")
         expect(actual).to.eql(expected) 
-    }
-    it("returns null if student is not found in array"), ()=>{
-        const inputArray = [
-            { name: "Leo Yeon-Joo", score: 8.9 },
-            { name: "Morgan Sutton", score: 7.4 },
-            { name: "Natalee Vargas", score: 9.2 }
-        ];
-        const actual = findStudentByName(inputArray, "Terry Peters")
+    });
+
+    it("returns null if student is not found in array", ()=>{
+        const actual = findStudentByName(input, "Terry Peters")
         expect(actual).to.be.null
-    }
-    it("returns null if array is empty"), ()=>{
-        const inputArray = [
-        ];
-        const actual = findStudentByName(inputArray, "Terry Peters")
+    });
+
+    it("returns null if array is empty", ()=>{
+
+        const actual = findStudentByName( [] , "Terry Peters")
         expect(actual).to.be.null
-    }
-})
+    });
+});
